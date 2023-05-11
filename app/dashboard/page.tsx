272eda3d367eff85d34615a06939f222ecf09c8b
Somojit Arambam <somoarambam123@gmail.com>
2023-05-11 16:22:27 +0530

    dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,6 @@
 import { useSession } from "next-auth/react";
 import Heading from "../components/Heading";
-<<<<<<< HEAD
-// import Tiptap from "../components/TipTap";
-=======
 import { getAllSubLinks } from "../actions/getAllSubLinks";
->>>>>>> 3c9d3e0432a99e9c6af9fd1207e4e2ddcd90ab2a
 
 const Dashboard = async () => {
   // const { data: session } = useSession();
@@ -18,10 +14,6 @@ const Dashboard = async () => {
         title="Welcome to Rely CMS 2"
         // subtitle={`Hello, ${session?.user?.name}`}
       />
-<<<<<<< HEAD
-      {/* <Tiptap /> */}
-=======
->>>>>>> 3c9d3e0432a99e9c6af9fd1207e4e2ddcd90ab2a
     </div>
   );
 };
