462c8e9a739615aecb0f537a3ba7faaf5eb18f5c
Somojit Arambam <somoarambam123@gmail.com>
2023-05-22 11:44:55 +0530

    navlinks added

--- app/actions/getAllAlbulms.ts
+++ app/actions/getAllAlbulms.ts
@@ -1,15 +1,6 @@
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-
 export async function getAllAlbulms(): Promise<AlbumProps[]> {
-  const session = await getServerSession(authOptions);
-
   const response: Response = await fetch(`${process.env.API_URL}/api/albulm`, {
     cache: "no-store",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: session?.user?.accessToken,
-    },
   });
   return response.json();
 }
